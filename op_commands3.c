9c30295511463eaf9e8643ba95b3505eb0f65f63
youssef <youssefelghamour@gmail.com>
2023-11-15 05:16:49 -0800

    add _pstr function in op_commands3.c

--- op_commands3.c
+++ op_commands3.c
@@ -27,3 +27,29 @@ void _pchar(stack_t **stack, unsigned int line_number)
 	c = (char)num;
 	dprintf(1, "%c\n", c);
 }
+
+/**
+ * _pstr - prints the string starting at the top of the stack, followed
+ * by a new line
+ *
+ * @stack: pointer to top of the stack
+ * @line_number: number of the line to be executed
+ */
+void _pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr = *stack;
+
+	if (ptr == NULL)
+	{
+		dprintf(1, "\n");
+		return;
+	}
+
+	while (ptr && ptr->n != 0 && (ptr->n > 0 && ptr->n <= 127))
+	{
+		dprintf(1, "%c", (char)ptr->n);
+		ptr = ptr->next;
+	}
+
+	dprintf(1, "\n");
+}
