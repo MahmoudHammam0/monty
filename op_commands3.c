6816d1bfa2386c7b6b3160cb12b782fab613a87c
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-11-15 08:18:18 -0800

    11. pchar

--- op_commands3.c
+++ op_commands3.c
@@ -17,7 +17,7 @@ void _pchar(stack_t **stack, unsigned int line_number)
 		dprintf(2, "L%u: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if (num > 127 || num < 0)
+	if (num < 0 || num > 127)
 	{
 		dprintf(2, "L%u: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
