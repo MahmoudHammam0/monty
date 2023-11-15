a8b5ff5caafa9585c6a4335a1ae853925a39ed04
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-11-14 22:02:04 -0800

    5. nop

--- execute_command.c
+++ execute_command.c
@@ -22,7 +22,7 @@ void execute_command(char *line, stack_t **stack, unsigned int line_number)
 	};
 
 	opc = strtok(line, " \n\t");
-	if (strlen(opc) != 0 && opc[0] == '#')
+	if (opc && opc[0] == '#')
 		return;
 	if (opc == NULL || strcmp(opc, "nop") == 0)
 		return;
