3def99f02ad3c6ae0cf50132c0afd2dc3c58f2ff
youssef <youssefelghamour@gmail.com>
2023-11-15 04:42:54 -0800

    try ascii range 32-126

--- op_commands3.c
+++ op_commands3.c
@@ -18,7 +18,7 @@ void _pchar(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	if (num < 0 || num > 127)
+	if (num < 32 || num > 126)
 	{
 		dprintf(2, "L%u: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
