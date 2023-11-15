04d603cf21a5dfd792e14d923d0accdf6903a6ca
youssef <youssefelghamour@gmail.com>
2023-11-15 01:46:49 -0800

    add _mul function in op_commands2.c

--- op_commands2.c
+++ op_commands2.c
@@ -81,3 +81,29 @@ void _div(stack_t **stack, unsigned int line_number)
         *stack = ptr2;
         free(ptr1), ptr1 = NULL;
 }
+
+/**
+ * _mul - multiplies the second top element of the stack with the top element
+ * of the stack
+ *
+ * @stack: pointer to top of the stack
+ * @line_number: number of line to be executed
+ */
+void _mul(stack_t **stack, unsigned int line_number)
+{
+        stack_t *ptr1 = *stack, *ptr2;
+        int num1, num2;
+
+        if (ptr1 == NULL || ptr1->next == NULL)
+        {
+                dprintf(2, "L%u: can't mul, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+        ptr2 = ptr1->next;
+        num1 = ptr1->n;
+        num2 = ptr2->n;
+        ptr2->n = num2 * num1;
+        ptr2->prev = NULL;
+        *stack = ptr2;
+        free(ptr1), ptr1 = NULL;
+}
