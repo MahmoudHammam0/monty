9c365f23c0d0533422f36ddaf0692039b759ab31
youssef <youssefelghamour@gmail.com>
2023-11-15 04:21:02 -0800

    add _pchar function in op_commands3.c

---
+++ op_commands3.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * _pchar - prints the char at the top of the stack, followed by a new line
+ *
+ * @stack: pointer to top of the stack
+ * @line_number: number of the line to be executed
+ */
+void _pchar(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr = *stack;
+	int num = ptr->n;
+	char c;
+
+	c = (char)num;
+
+	if (ptr == NULL)
+	{
+		dprintf(2, "L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if (num < 0 || num > 127)
+	{
+		dprintf(2, "L%u: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	dprintf(1, "%c\n", num);
+}
